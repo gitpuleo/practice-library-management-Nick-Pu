9ec2f01d3f3fce87f1135c84de2a8f4bb6e1b074
nfp <nfpuleo@gmail.com>
2025-05-25 01:14:13 -0500

    outlining code

--- inheritance-task.js
+++ inheritance-task.js
@@ -1,1 +1,40 @@
-//catching up
+class LibraryItem{
+    constructor(){
+        this.title = title;
+        this.id = id;
+        this.isAvailable = isAvailable;
+    }
+    checkOut() {
+        //code here
+    }
+    returnItem() {
+        //someCode
+    }
+
+}
+
+class Book extends LibraryItem {
+    constructor() {
+        super();
+        this.genre = genre;
+        this.author = author;
+    }
+}
+
+class DVD extends LibraryItem {
+    constructor() {
+        super();
+        this.director = director;
+        this.duration = duration;
+    }
+}
+
+class Magazine extends LibraryItem {
+    constructor() {
+        super();
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
+}
+
+let fearAndLoathing = new Book()
