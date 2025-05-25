c29f12d533bf4762dd720241676dddf2355da235
nfp <nfpuleo@gmail.com>
2025-05-25 01:52:27 -0500

    Tasks completed

--- inheritance-task.js
+++ inheritance-task.js
@@ -1,40 +1,60 @@
 class LibraryItem{
-    constructor(){
+    constructor(title, id, isAvailable){
         this.title = title;
         this.id = id;
         this.isAvailable = isAvailable;
     }
     checkOut() {
-        //code here
+        this.isAvailable = false; 
+        console.log(`${this.title} is now available: ${this.isAvailable}.`)
     }
     returnItem() {
-        //someCode
+        this.isAvailable = true;
+        console.log(`${this.title} is now available: ${this.isAvailable}.`)
     }
 
 }
 
 class Book extends LibraryItem {
-    constructor() {
-        super();
+    constructor(title, id, isAvailable, genre, author) {
+        super(title, id, isAvailable);
         this.genre = genre;
         this.author = author;
     }
 }
 
 class DVD extends LibraryItem {
-    constructor() {
-        super();
+    constructor(title, id, isAvailable, director, duration) {
+        super(title, id, isAvailable);
         this.director = director;
         this.duration = duration;
+    }
+    summarizeFilm(){
+        console.log(`The movie ${this.title} by ${this.director} is ${this.duration} long. You should watch it!`)
     }
 }
 
 class Magazine extends LibraryItem {
-    constructor() {
-        super();
+    constructor(title, id, isAvailable, issueNumber, publisher) {
+        super(title, id, isAvailable);
         this.issueNumber = issueNumber;
         this.publisher = publisher;
     }
 }
 
-let fearAndLoathing = new Book()
+let fearAndLoathing = new Book("Fear and Loathing in Las Vegas", 744, true, "non-fiction", "Hunter S. Thomnpson")
+console.log(fearAndLoathing);
+
+let firstDVD = new DVD("Moe no Suzaku", 777, false, "Naomi Kawase", "1h34m")
+console.log(firstDVD);
+
+let firstMagazine = new Magazine("Monocle", 333, true, 42, "Winkreative Ltd.")
+console.log(firstMagazine);
+
+
+firstMagazine.checkOut();
+firstDVD.returnItem();
+firstDVD.summarizeFilm();
+
+console.log(firstDVD.director);
+console.log(fearAndLoathing.genre);
